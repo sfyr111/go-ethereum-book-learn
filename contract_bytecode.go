222cc5fdbc4c3a598adffe7f3b5a9425952ac931
sfyr111 <sfyr111@163.com>
2025-02-10 00:07:07 +0800

    feat: add contract bytecode retrieval script for Store smart contract

---
+++ contract_bytecode.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contractAddress := common.HexToAddress("0xf3585FCD969502624c6A8ACf73721d1fce214E83")
+	bytecode, err := client.CodeAt(context.Background(), contractAddress, nil) // nil is latest block
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(hex.EncodeToString(bytecode)) // 60806...10029
+}
