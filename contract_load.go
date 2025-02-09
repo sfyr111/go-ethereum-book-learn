7bc772ab957663ce25b49fec637d41287041625b
sfyr111 <sfyr111@163.com>
2025-02-09 21:13:18 +0800

    feat: add contract loading script for Store smart contract

---
+++ contract_load.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	store "go-ethereum-book-learn/contract/store"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0x91F806EDD78785820Ff55b88C10De7D273e76C5e")
+	instance, err := store.NewStore(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("contract is loaded")
+	_ = instance
+}
